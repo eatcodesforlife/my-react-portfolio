fed197a260e90dfa47eb0a0554523d3b333f598c
Cris Acosta <iamcris87@gmail.com>
2021-08-27 12:26:07 +0400

    updating success page router

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -14,7 +14,7 @@ const Connect = () => {
                 <form 
                     className='form' 
                     name='contact' 
-                    action='/thankyou' 
+                    action='/thankyou/' 
                     method="post"
                     data-netlify='true'
                     onSubmit='submit'
